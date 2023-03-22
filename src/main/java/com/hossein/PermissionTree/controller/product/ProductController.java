6050ca2a6f68f113cefe717ef155356df8daad9a
mhfCodes <mohammadhossein.farrokhi.78@gmail.com>
2023-03-23 01:53:25 +0430

    added product fetching in controller

--- src/main/java/com/hossein/PermissionTree/controller/product/ProductController.java
+++ src/main/java/com/hossein/PermissionTree/controller/product/ProductController.java
@@ -1,12 +1,37 @@
 package com.hossein.PermissionTree.controller.product;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hossein.PermissionTree.controller.viewModel.ProductViewModel;
+import com.hossein.PermissionTree.dto.product.ProductDto;
+import com.hossein.PermissionTree.mapper.Product.ProductMapper;
+import com.hossein.PermissionTree.service.Product.IProductService;
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api")
 public class ProductController {
 
+	@Autowired
+	private ProductMapper mapper;
+	@Autowired
+	private IProductService iProductService;
+	
+	@GetMapping("/product")
+	public List<ProductViewModel> getAll() {
+		return this.mapper.mapEtoVList(this.iProductService.getAllProducts());
+	}
+	
+	@PostMapping("/product/search")
+	public List<ProductViewModel> search(@RequestBody ProductDto data) {
+		return this.iProductService.search(data);
+	}
 	
 	
 }
