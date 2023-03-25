2d4f694ea15db0deee3d7a51352a34e03c36106c
mhfCodes <mohammadhossein.farrokhi.78@gmail.com>
2023-03-26 04:19:44 +0430

    added product ui buttons

--- src/main/resources/static/script/product.js
+++ src/main/resources/static/script/product.js
@@ -6,7 +6,7 @@
         const products = await fetchProducts();
         products.forEach(product => {
             const tr = document.createElement("tr");
-            tr.innerHTML = `<td>${product.productName}</td><td>${product.productPrice}</td><td>${product.productCount}</td><td>${product.productDateAdded}</td>`;
+            tr.innerHTML = `<td>${product.productName}</td><td>${product.productPrice}</td><td>${product.productCount}</td><td>${product.productDateAdded}</td><td><button class="btn btn-small btn-edit">Edit</button><button class="btn btn-small btn-delete">Delete</button></td>`;
             tableBody.insertAdjacentElement("beforeend", tr);
         });
     }
