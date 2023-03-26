4ed9dd383ac22ab6f645a0b7a76b18a448838ef1
mhfCodes <mohammadhossein.farrokhi.78@gmail.com>
2023-03-27 02:02:44 +0430

    added product search ui and functionality

--- src/main/resources/static/script/product.js
+++ src/main/resources/static/script/product.js
@@ -1,15 +1,24 @@
 (function() {
-
+    
     const tableBody = document.querySelector(".table-body");
-    const addBtn = document.querySelector(".btn-add");
     const modal = document.querySelector(".modal");
     const overlay = document.querySelector(".overlay");
+    const searchBox = document.querySelector(".search-box");
+
+    const addBtn = document.querySelector(".btn-add");
     const closeBtn = document.querySelector(".btn-close");
     const saveBtn = document.querySelector(".btn-submit");
+    const searchBtn = document.querySelector(".btn-search");
+    const findBtn = document.querySelector(".btn-find");
 
     const nameInput = document.getElementById("name");
     const priceInput = document.getElementById("price");
     const countInput = document.getElementById("count");
+
+    const searchNameInput = document.getElementById("search-name");
+    const searchPriceInput = document.getElementById("search-price");
+    const searchCountInput = document.getElementById("search-count");
+    const searchDateInput = document.getElementById("search-date");
 
     const init = async () => {
         const products = await fetchProducts();
@@ -59,7 +68,47 @@
         }
     }
 
+    const find = async () => {
+
+        if (searchPriceInput.value < 0) {
+            alert("Price can not be less than 0");
+            throw new Error("Price can not be less than 0");
+        }
+
+        if (searchCountInput.value < 0) {
+            alert("Count can not be less than 0");
+            throw new Error("Count can not be less than 0");
+        }
+
+        const searchProductObj = {
+            id: null,
+            name: searchNameInput.value,
+            price: parseFloat(searchPriceInput.value),
+            count: parseInt(searchCountInput.value),
+            dateAdded: searchDateInput.value
+        };
+
+        const response = await fetch ("http://localhost:8080/api/product/search", {
+            method: 'POST',
+            body: JSON.stringify(searchProductObj),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        const data = await response.json();
+
+        tableBody.innerHTML = "";
+        data.forEach(product => {
+            const tr = document.createElement("tr");
+            tr.innerHTML = `<td>${product.productName}</td><td>${product.productPrice}</td><td>${product.productCount}</td><td>${product.productDateAdded}</td><td><button class="btn btn-small btn-edit">Edit</button><button class="btn btn-small btn-delete">Delete</button></td>`;
+            tableBody.insertAdjacentElement("beforeend", tr);
+        });
+    }
+
     document.addEventListener('DOMContentLoaded', init);
+    saveBtn.addEventListener('click', saveProduct);
+    findBtn.addEventListener('click', find);
+
     addBtn.addEventListener('click', () => {
         modal.classList.remove("hidden");
         overlay.classList.remove("hidden");
@@ -68,6 +117,7 @@
         modal.classList.add("hidden");
         overlay.classList.add("hidden");
     })
-    saveBtn.addEventListener('click', saveProduct);
-    
+    searchBtn.addEventListener('click', () => {
+        searchBox.classList.toggle("hidden");
+    });
 })();
