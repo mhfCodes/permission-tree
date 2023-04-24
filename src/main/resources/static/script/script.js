29efe7fee7c19e2d9222dcd8e3d057e796cb8599
mhfCodes <mohammadhossein.farrokhi.78@gmail.com>
2023-04-24 23:15:39 +0430

    added management dropdown ui and functionality

--- src/main/resources/static/script/script.js
+++ src/main/resources/static/script/script.js
@@ -5,6 +5,11 @@
     const logoutBtn = document.querySelector(".logout");
     const navSignup = document.querySelector(".nav-signup");
     const navProducts = document.querySelector(".nav-products");
+    const navManagement = document.querySelector(".nav-management");
+    const dropdownBtn = document.querySelector(".btn-dropdown");
+    const dropdownContent = document.querySelector(".dropdown-content");
+    const arrowUp = document.getElementById("arrow-up");
+    const arrowDown = document.getElementById("arrow-down");
 
     const fetchHome = async () => {
 
@@ -13,11 +18,19 @@
             navProducts.classList.remove("hidden");
             navSignup.classList.add("hidden");
             navLogout.classList.remove("hidden");
+            navManagement.classList.remove("hidden");
         }
 
         const response = await fetch("http://localhost:8080");
         const data = await response.text();
         document.getElementById("date").textContent = data;
+    }
+
+    const dropdownContentToggle = () => {
+        dropdownContent.classList.toggle("hidden");
+        arrowUp.classList.toggle("hidden");
+        arrowDown.classList.toggle("hidden");
+
     }
 
     const logout = () => {
@@ -26,6 +39,7 @@
     }
 
     document.addEventListener('DOMContentLoaded', fetchHome);
+    dropdownBtn.addEventListener('click', dropdownContentToggle);
     logoutBtn.addEventListener('click', logout);
 
 })();
