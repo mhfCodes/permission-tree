76ebe1f61488fdd2d827fda9de82732541058058
mhfCodes <mohammadhossein.farrokhi.78@gmail.com>
2023-07-07 15:30:30 +0430

    added values for OrderStatus Enum

--- src/main/java/com/hossein/PermissionTree/model/order/OrderStatus.java
+++ src/main/java/com/hossein/PermissionTree/model/order/OrderStatus.java
@@ -1,5 +1,16 @@
 package com.hossein.PermissionTree.model.order;
 
 public enum OrderStatus {
-	CANCELED, COMPLETED, FAILED, ONHOLD, PROCESSING
+	CANCELED(0), COMPLETED(1), FAILED(2), ONHOLD(3), PROCESSING(4);
+	
+	private int orderStatusValue;
+	
+	private OrderStatus(int orderStatusValue) {
+		this.orderStatusValue = orderStatusValue;
+	}
+
+	public int getOrderStatusValue() {
+		return orderStatusValue;
+	}
+
 }
